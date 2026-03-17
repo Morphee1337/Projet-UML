a19387e1f3f9cff48c63797053ee32da862349cd
Morphee1337 <arthur.israel@edouardgand.fr>
2026-03-17 09:17:16 +0100

    Exo 6 inheritance: Media/Series

--- uml-cinema-training/Program.cs
+++ uml-cinema-training/Program.cs
@@ -1,16 +1,5 @@
 ﻿using UmlCinemaTraining;
 
-// Exercise 5: Studio + Playlist
-var studio = new Studio("CineArt", "Lyon");
-var movie1 = new Movie("Le Voyage", 125, studio);
-var movie2 = new Movie("Lumières", 98, studio);
-
-var playlist = new Playlist("Favorites");
-playlist.AddMovie(movie1);
-playlist.AddMovie(movie2);
-
-playlist.DisplayMovies();
-
-// Display studio for each movie
-Console.WriteLine($"Studio for {movie1.Title}: {movie1.Studio?.Name}");
-Console.WriteLine($"Studio for {movie2.Title}: {movie2.Studio?.Name}");
+// Exercise 6: Inheritance (Media / Series)
+var series = new Series("Séries Infinies", 45, 4);
+series.Display();
