86036787deab7e26877396b9fcebd24d7febb805
Morphee1337 <arthur.israel@edouardgand.fr>
2026-03-17 09:13:42 +0100

    Exo 1 classe Movie

--- uml-cinema-training/Program.cs
+++ uml-cinema-training/Program.cs
@@ -1,23 +1,5 @@
 ﻿using UmlCinemaTraining;
 
-// Final exercise demo: viewer, studio, movies, and watchlist.
-var viewer = new Viewer("Emma", "Lacroix", "emma.lacroix@example.com");
-viewer.Display();
-
-var studio = new Studio("CineArt", "Lyon");
-studio.Display();
-
-var movie1 = new Movie("Le Voyage", 125, studio);
-var movie2 = new Movie("Lumières", 98, studio);
-
-movie1.Display();
-movie2.Display();
-
-var watchList = new WatchList("Favoris", viewer);
-watchList.AddMovie(movie1);
-watchList.AddMovie(movie2);
-watchList.DisplayMovies();
-
-// Demo inheritance (Media / Series)
-var series = new Series("Séries Infinies", 45, 4);
-series.Display();
+// Exercise 1: Movie
+var movie = new Movie("Inception", 148);
+movie.Display();
