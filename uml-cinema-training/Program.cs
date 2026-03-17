18a9ed946c4bc1632fbbfb9ff545d15f874e3abf
Morphee1337 <arthur.israel@edouardgand.fr>
2026-03-17 09:16:20 +0100

    Exo 4 Playlist with multiple movies

--- uml-cinema-training/Program.cs
+++ uml-cinema-training/Program.cs
@@ -1,6 +1,12 @@
 ﻿using UmlCinemaTraining;
 
-// Exercise 3: Movie with a Director
+// Exercise 4: Playlist with multiple movies
 var director = new Director("Christopher", "Nolan", "UK");
-var movie = new Movie("Inception", 148, director);
-movie.Display();
+var movie1 = new Movie("Inception", 148, director);
+var movie2 = new Movie("Tenet", 150, director);
+
+var playlist = new Playlist("Favorites");
+playlist.AddMovie(movie1);
+playlist.AddMovie(movie2);
+
+playlist.DisplayMovies();
